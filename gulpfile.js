7ac56059aad502b3f7459ed233d3a3fa891ae345
Robert <dungeonry@hotmail.com>
2016-05-27 12:44:54 -0700

    turns out the gulpfile wasn't working.  Made some changes to make it work and changed the gitignore to ignore the dist.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var imagemin = require("gulp-imagemin");
 var cache = require("gulp-cache");
 
 gulp.task("imagemin", function(){
-  return gulp.src("public/image/*.+(png|svg|jpg|gif)")
-    .pipe(cache(imagemin())
-    .pipe("dist/images");
+  return gulp.src("public/images/*.+(png|svg|jpg|gif)")
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest("dist/images"));
 });
